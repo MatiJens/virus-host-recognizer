9194160a49233262c93e594f89f77f414a084ca1
Mateusz <mateuszjens14@gmail.com>
2025-12-01 14:19:37 +0100

    rf cl vs normal

--- model_training.py
+++ model_training.py
@@ -3,16 +3,20 @@ from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
+from sklearn.model_selection import GroupShuffleSplit
 
-data_path = "data/processed/concated_data.pkl"
-
+data_path = "data/processed/contrasted_learning_protbert.pkl"
 data = pd.read_pickle(data_path)
 
 X = np.stack(data["embedding"].values)
 y = data["label"]
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, random_state=42
-)
+groups = data["virus_group"]
+
+splitter = GroupShuffleSplit(test_size=0.20, n_splits=1, random_state=42)
+train_idx, test_idx = next(splitter.split(X, y, groups))
+
+X_train, X_test = X[train_idx], X[test_idx]
+y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
 
 
 model_rf = RandomForestClassifier(
@@ -29,7 +33,7 @@ y_pred = model_rf.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
 report = classification_report(y_test, y_pred)
 
-results_path = "results.txt"
+results_path = "cl_results.txt"
 
 with open(results_path, "w", encoding="utf-8") as f:
     f.write("Confusion Matrix:\n")
